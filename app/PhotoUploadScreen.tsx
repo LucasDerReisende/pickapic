493faa869753fa98c7b5c87a8fd8c5101afe949f
Lucas Reisener <lucasreisener@gmail.com>
2023-09-07 15:00:07 +0200

    create initial pages

---
+++ app/PhotoUploadScreen.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+
+export default function PhotoUploadScreen() {
+    return (
+        <View>
+            <Text>Photo Upload Screen</Text>
+        </View>
+    )
+}
---
+++ app/PinEntryScreen.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+
+export default function PinEntryScreen() {
+    return (
+        <View>
+            <Text>Pin Entry Screen</Text>
+        </View>
+    )
+}
